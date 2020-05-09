db0902898d47fd5b4818301ec4759d7925dc7237
Stephen O'Connor <stephen.oconnor@collectivworks.com>
2020-05-09 09:47:28 +0100

    Added project configuration.

---
+++ src/components/App/App.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { hot } from 'react-hot-loader';
+import img from './image_cat.jpg';
+import s from './styles.module.scss';
+
+export interface AppProps {
+	compiler: string;
+	framework: string;
+}
+
+class App extends React.Component<AppProps, {}> {
+	render(): React.ReactNode {
+		return (
+			<main role="main" className={s.heading}>
+				<h1>
+					{this.props.framework} App with {this.props.compiler}
+				</h1>
+				<img src={img} alt="Cat" />
+			</main>
+		);
+	}
+}
+
+export default hot(module)(App);
---
+++ webpack.config.prod.js
@@ -0,0 +1,65 @@
+const merge = require('webpack-merge');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const baseConfig = require('./webpack.config.base');
+
+module.exports = merge(baseConfig, {
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.module\.s(a|c)ss$/,
+				loader: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							modules: {
+								localIdentName: '[hash:base64]',
+							},
+							sourceMap: false,
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: false,
+						},
+					},
+				],
+			},
+			{
+				test: /\.s(a|c)ss$/,
+				exclude: /\.module.(s(a|c)ss)$/,
+				loader: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'sass-loader',
+						options: {
+							modules: {
+								localIdentName: '[hash:base64]',
+							},
+							sourceMap: false,
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new BundleAnalyzerPlugin({
+			analyzerMode: 'static',
+			openAnalyzer: false,
+			reportFilename: 'bundle-sizes.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].[hash].css',
+			chunkFilename: '[id].[hash].css',
+		}),
+	],
+	externals: {
+		react: 'React',
+		'react-dom': 'ReactDOM',
+	},
+});
